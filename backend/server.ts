721b597f384d7b29870081161f31214130e30056
taslima <garryanderson337@gmail.com>
2026-08-21 18:37:26 +0600

    feat: implement custom HTTP server configuration with connection management and graceful shutdown logic

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import { app } from "@app";
 import { connectDb } from "@config/db.js";
+import { logger } from "@utils/logger.js";
 import { resolve } from "node:dns";
 import { createServer, type Server } from "node:http";
 
@@ -7,27 +8,42 @@ const connection_checking_interval = 5000;
 const keep_alive_timeout = 65000;
 const headers_timeout = 30000;
 const req_timeout = 30000;
+const idle_sweep_interval = 1000
 
 let shuttingDown = false;
 let server: Server | null = null;
-let httpClosePromise : Promise<void> | null = null
+let httpClosePromise: Promise<void> | null = null;
 
-const closeHttpServer= async() : Promise <void> =>{
-  if(httpClosePromise) return httpClosePromise
+const closeHttpServer = async (): Promise<void> => {
+  if (httpClosePromise) return httpClosePromise;
 
-  const activeServer = server
-  if(activeServer?.listening) return
-}
-const listen = (httpServer : Server, port : number)=>{
-  return new Promise <void> ((resolve, reject)=>{
-    httpServer.once('error',reject)
-    httpServer.listen(port,()=>{
-      httpServer.removeListener('error',reject)
-      resolve()
-    })
-    
-  })
-}
+  const activeServer = server;
+  if (activeServer?.listening) return;
+  httpClosePromise = (async():Promise <void>=>{
+    const idleSweeper = setInterval(()=>{
+      activeServer?.closeIdleConnections
+    },idle_sweep_interval)
+    try{
+      await new Promise <void>((resolve, reject)=>{
+        activeServer?.close(err=>(err? reject(err) : resolve()))
+      })
+    }finally{
+      clearInterval(idleSweeper)
+    }
+    logger.info('http server closed')
+
+  })()
+  return httpClosePromise
+};
+const listen = (httpServer: Server, port: number) => {
+  return new Promise<void>((resolve, reject) => {
+    httpServer.once("error", reject);
+    httpServer.listen(port, () => {
+      httpServer.removeListener("error", reject);
+      resolve();
+    });
+  });
+};
 
 const startServer = async (): Promise<void> => {
   await connectDb();
@@ -44,5 +60,5 @@ const startServer = async (): Promise<void> => {
   httpServer.keepAliveTimeout = keep_alive_timeout;
   httpServer.headersTimeout = headers_timeout;
   httpServer.requestTimeout = req_timeout;
-  if(shuttingDown) return
+  if (shuttingDown) return;
 };
